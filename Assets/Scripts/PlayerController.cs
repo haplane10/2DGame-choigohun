3fabae661dc8966ca078b0eacbd83e4e22aef991
haplane5 <haplane5@gmail.com>
2020-09-08 21:30:13 +0900

    2dgame1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    public float speed;
+    public new Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        if (direction != 0)
+        {
+            animator.SetBool("Running", true);
+            rigidbody.velocity = new Vector2(speed * direction, 0);
+
+            if (direction > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            // 내용 입력
+        }
+
+    }
+}
