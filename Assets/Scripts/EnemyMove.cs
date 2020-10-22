8141200e7d2bf96e9e222d077469fd0aea2c4a5a
haplane5 <haplane5@gmail.com>
2020-10-22 11:23:56 +0900

    3수업

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    [SerializeField] private float defaultSpeed = 2.5f;
+    private float speed;
+    public float direction;
+    private Animator animator;
+    private PlayerController playerController;
+    private bool isAttack = false;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        speed = defaultSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(playerController.transform.position, transform.position);
+        
+        if (distance <= 0.75f)
+        {
+            speed = 0f;
+            animator.SetBool("isWalk", false);
+            animator.SetTrigger("Attack");
+        }
+        else
+        {
+            speed = defaultSpeed;
+            animator.SetBool("isWalk", true);
+        }
+
+        direction = playerController.transform.position.x - transform.position.x;
+        direction = Mathf.Clamp(direction, -1, 1);
+        rigidbody.velocity = new Vector2(speed * direction, rigidbody.velocity.y);
+
+        if (isAttack == false)
+        {
+            animator.SetBool("isWalk", true);
+        }
+        
+        // 캐릭터 뒤돌기
+        if (direction < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,10 +12,18 @@ public class PlayerController : MonoBehaviour
 
     public bool isTile;
 
+    //int A, B = 30, C = 70;
+
+    //int Add(int D, int E)
+    //{
+    //    return D + E;
+    //}
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //A = Add(B, C);
+        //Debug.Log(A);
     }
 
     // Update is called once per frame
@@ -27,6 +35,8 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("Running", true);
             rigidbody.velocity = new Vector2(speed * direction, rigidbody.velocity.y);
 
+
+            // 캐릭터 뒤돌기
             if (direction > 0)
             {
                 transform.localScale = new Vector3(1, 1, 1);
@@ -51,19 +61,24 @@ public class PlayerController : MonoBehaviour
         {
             // 내용 입력
         }
-
     }
 
-    //private void FixedUpdate()
-    //{
-    //    rigidbody.AddForce(Physics.gravity * rigidbody.mass);
-    //}
-
-    private void OnCollisionStay2D(Collision2D collision)
+    private void FixedUpdate()
     {
-        if (isTile == false && collision.gameObject.CompareTag("Tile"))
+        Debug.DrawRay(transform.position, Vector3.down, new Color(1, 0, 0));  // (0, -1, 0)
+        RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, Vector3.down, 1, LayerMask.GetMask("Tile"));
+        if (raycastHit.collider != null)
         {
             isTile = true;
+            Debug.Log("충돌되었습니다.");
         }
     }
+
+    //private void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    if (isTile == false && collision.gameObject.CompareTag("Tile"))
+    //    {
+    //        isTile = true;
+    //    }
+    //}
 }
