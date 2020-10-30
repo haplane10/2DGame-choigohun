3c3919ed143391953e91aec54333b85836529186
haplane5 <haplane5@gmail.com>
2020-10-30 18:26:04 +0900

    애니메이션 수정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,29 +29,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float direction = Input.GetAxis("Horizontal");
+        float direction = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Direction", direction);
 
         if (direction != 0)
         {
-            animator.SetBool("Running", true);
+            if (isTile)
+            {
+                animator.SetInteger("Animation", 2);
+            }
             rigidbody.velocity = new Vector2(speed * direction, rigidbody.velocity.y);
+            transform.localScale = new Vector3(direction, 1, 1);
         }
-        else
+        
+        if (isTile && Input.GetAxis("Jump") > 0)
         {
-            animator.SetBool("Running", false);
-        }
-
-        if (isTile == true && Input.GetAxis("Jump") > 0)
-        {
-            animator.SetTrigger("Jumping");
-            isTile = false;
             rigidbody.AddForce(Vector2.up * jump);
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.Z))
         {
-            animator.SetTrigger("Attack");
+            animator.SetInteger("Animation", 4);
         }
     }
 
@@ -62,23 +60,29 @@ public class PlayerController : MonoBehaviour
         if (raycastHit.collider != null)
         {
             isTile = true;
+            animator.SetInteger("Animation", 0);
             Debug.Log("충돌되었습니다.");
         }
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Tile"))
+        else
         {
-            animator.SetBool("isGround", true);
+            isTile = false;
+            animator.SetInteger("Animation", 3);
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Tile"))
-        {
-            animator.SetBool("isGround", false);
-        }
-    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Tile"))
+    //    {
+    //        animator.SetInteger("Animation", 0);
+    //    }
+    //}
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Tile"))
+    //    {
+    //        animator.SetInteger("Animation", 3);
+    //    }
+    //}
 }
